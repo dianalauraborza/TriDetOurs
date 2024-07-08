fd869441fa40729bb1f0d4a691649ac6855fea92
dia <diana.borza@inria.fr>
2024-07-08 10:28:45 +0200

    summary rpoject

--- libs/modeling/blocks.py
+++ libs/modeling/blocks.py
@@ -257,7 +257,7 @@ class SGPBlock(nn.Module):
         self.gating_mechanism = GatingMechanism(n_embd, 32)
 
         self.summarization = TokenSummarizationMHA(64, n_embd)
-        self.summary_project = nn.Conv1d(n_embd, n_embd, stride=1, padding=0, groups=n_embd)
+        self.summary_project = nn.Conv1d(n_embd, n_embd, 1, stride=1, padding=0, groups=n_embd)
         self.summary_fc = nn.Conv1d(n_embd, n_embd, 1, stride=1, padding=0, groups=n_embd)
 
         # input
