1608246b381afa842206b144db23979f62fb064b
Aglind Reka <aglindreka@live.com>
2024-07-13 10:04:22 +0200

    modifications

--- tools/ant_tsp_script.sh
+++ tools/ant_tsp_script.sh
@@ -1,6 +1,6 @@
 ##!/bin/bash
 
 echo "start training"
-CUDA_VISIBLE_DEVICES=$1 python train.py ./configs/anet_tsp.yaml --output pretrained
+CUDA_VISIBLE_DEVICES=$1 python train.py ./configs/anet_tsp.yaml --output anet_46_3GPU
 echo "start testing..."
-CUDA_VISIBLE_DEVICES=$1 python eval.py ./configs/anet_tsp.yaml ckpt/anet_tsp_pretrained/epoch_014.pth.tar
+CUDA_VISIBLE_DEVICES=$1 python eval.py ./configs/anet_tsp.yaml ckpt/anet_tsp_anet_46_3GPU/epoch_029.pth.tar
